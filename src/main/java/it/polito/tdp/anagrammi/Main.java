7c5138da79ad41d66106e80d7cf126f7b701ce87
FabioMau <maulucci.fabio@gmail.com>
2022-06-28 23:33:36 +0200

    Anagramma

--- src/main/java/it/polito/tdp/anagrammi/Main.java
+++ src/main/java/it/polito/tdp/anagrammi/Main.java
@@ -2,6 +2,25 @@ package it.polito.tdp.anagrammi;
 
 public class Main {
 	public static void main(String[] args) {
-        EntryPoint.main(args);
-    }
+		//EntryPoint.main(args);
+		anagramma("ciao");
+	}
+
+	public static void anagramma(String s) {
+		anagramma_ricorsivo("", 0, s);
+	}
+
+	public static void anagramma_ricorsivo(String parziale, int livello, String rimanenti) {
+		
+		if(rimanenti.length() == 0) {
+			System.out.println(parziale);
+			return;
+		}
+		
+		for(int i = 0; i<rimanenti.length(); i++ )  {
+			anagramma_ricorsivo(parziale+rimanenti.charAt(i), livello+1, rimanenti.substring(0, i)+rimanenti.substring(i+1));
+		}
+		
+	}
+
 }
